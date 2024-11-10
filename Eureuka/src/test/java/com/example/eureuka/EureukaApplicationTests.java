43d2c4d01b582ba0361d3220c416e949cc82e986
nourfriha <nourfriha2@gmail.com>
2024-11-10 14:39:35 +0100

    gateway+eureka+reclamation

---
+++ Eureuka/src/test/java/com/example/eureuka/EureukaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureuka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EureukaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,26 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+//@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("reclamation", r -> r.path("/reclamations/**")
+                        .uri("http://localhost:8080"))  // reclamation : 8080
+                .build();
+    }
+}
